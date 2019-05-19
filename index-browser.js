35238826445785eaadf97b3b526c13a80be550b6
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-05-19 18:23:00 +0000

    feat: working browser version

--- index-browser.js
+++ index-browser.js
@@ -1,38 +1,38 @@
 const ci = require('@ipld/codec-interface')
-const cache = {}
-const convert = (c, name) => ci.create(c.util.serialize, c.util.deserialize, name)
+const util = require('multicodec/src/util')
+const table = require('multicodec/src/name-table')
 
+const basename = num => table[util.numberToBuffer(num).toString('hex')]
+
+let globals
+if (typeof window === 'undefined') globals = this
+else globals = window
+
+if (!globals.codecCache) {
+  globals.codecCache = {}
+}
+
+const cache = globals.codecCache
+
+const _convert = c => ci.create(c.util.serialize, c.util.deserialize, basename(c.codec))
+
+cache['dag-json'] = require('@ipld/dag-json')
+cache['dag-cbor'] = _convert(require('ipld-dag-cbor'))
+cache['dag-pb'] = _convert(require('ipld-dag-pb'))
 cache.raw = {
   encode: x => x,
   decode: x => x,
   codec: 'raw'
-  // no reader, you can't read raw blocks
 }
 
-/* temp getFormat until the real one is implemented */
 const getCodec = async codec => {
   if (cache[codec]) return cache[codec]
-  let resolve = mod => {
-    cache[codec] = mod.default
-    return cache[codec]
-  }
-  let save = c => { 
-    cache[codec] = c 
-    return c
-  }
-  let _convert = m => save(convert(m.default))
-  if (codec === 'dag-cbor') {
-    cache[codec] = import('ipld-dag-cbor').then(_convert)
-  }
-  if (codec === 'dag-json') {
-    cache[codec] = import('@ipld/dag-json').then(resolve)
-  }
-  if (cache[codec]) return cache[codec]
-  else throw new Error(`Unknown codec ${codec}`)
+  throw new Error(`Unknown codec ${codec}`)
 }
 
 module.exports = getCodec
+
 module.exports.setCodec = codec => {
+  if (codec.util) codec = _convert(codec)
   cache[codec.codec] = codec
 }
-
